21e1a167ae039443288ca0c61386ac540aaed775
Leniel Bello <yamichen62@gmail.com>
2025-10-15 11:36:17 -0400

    Carpetas y archivos movidos

--- app.py
+++ app.py
@@ -345,4 +345,5 @@ def eliminar_media(proyecto_id, media_index):
     return redirect(url_for('editar', id=proyecto_id))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
